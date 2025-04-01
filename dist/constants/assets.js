3aabcf6e6198418cc736dab2eb9dc384e4f1284f
RichardTinsley <49978658+RichardTinsley@users.noreply.github.com.>
2025-04-01 14:34:04 +1000

    TYPESCRIPT DAY 14 ASSETS CLEAN UP

--- dist/constants/assets.js
+++ dist/constants/assets.js
@@ -6,41 +6,11 @@ const UI_URL = "./images/UI/";
 const LEVELS_URL = "./images/levels/";
 const MUSIC_URL = "./audio/";
 export const ALL_ASSETS = new Map();
-export const TYPES = {
-    NORMAL: "Plain",
-    MENUITEM: "MenuItem",
-    ENEMY: "Enemy",
-    TOWER: "Tower",
-    PROJECTILE: "Projectile",
-    EFFECT: "Effect",
-};
-export const TOWERS = {
-    TOPAZ: "topaz",
-    RUBY: "ruby",
-    SAPPHIRE: "sapphire",
-    EMERALD: "emerald",
-    AMETHYST: "amethyst",
-    CITRINE: "citrine",
-    SILVER: "silver",
-    GOLD: "gold",
-    DIAMOND: "diamond",
-    OBSIDIAN: "obsidian",
-    OPAL: "opal",
-    URANIUM: "uranium",
-};
-const URL_NUMBER = {
-    ONE: " (1)",
-    TWO: " (2)",
-    THREE: " (3)",
-    FOUR: " (4)",
-    FIVE: " (5)",
-    SIX: " (6)",
-};
 export const FILE_NAMES = {
     DSLOGO: "DsShieldLogo.png",
     LEVEL_LAVONEY: "lavoney.png",
     MUSIC_MAIN_MENU: "mainMenu.mp3",
-    TOWER_AMETHYST_1: `${TOWERS.AMETHYST}${TYPES.TOWER}${URL_NUMBER.ONE}.png`,
+    TOWER_AMETHYST_1: `amethystTower (1).png`,
     PROJECTILE_SAPPHIRE_1: "Iceball_84x9.png",
 };
 export const ASSET_LIST = [
--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -4,43 +4,9 @@ const PROJECTILES_URL: string = "./images/projectiles/";
 const TOWERS_URL: string = "./images/towers/";
 const UI_URL: string = "./images/UI/";
 const LEVELS_URL: string = "./images/levels/";
-
 const MUSIC_URL: string = "./audio/";
 
 export const ALL_ASSETS = new Map();
-
-export const TYPES = {
-  NORMAL: "Plain",
-  MENUITEM: "MenuItem",
-  ENEMY: "Enemy",
-  TOWER: "Tower",
-  PROJECTILE: "Projectile",
-  EFFECT: "Effect",
-};
-
-export const TOWERS = {
-  TOPAZ: "topaz",
-  RUBY: "ruby",
-  SAPPHIRE: "sapphire",
-  EMERALD: "emerald",
-  AMETHYST: "amethyst",
-  CITRINE: "citrine",
-  SILVER: "silver",
-  GOLD: "gold",
-  DIAMOND: "diamond",
-  OBSIDIAN: "obsidian",
-  OPAL: "opal",
-  URANIUM: "uranium",
-};
-
-const URL_NUMBER = {
-  ONE: " (1)",
-  TWO: " (2)",
-  THREE: " (3)",
-  FOUR: " (4)",
-  FIVE: " (5)",
-  SIX: " (6)",
-};
 
 // prettier-ignore
 export const FILE_NAMES: Record<string, string> = {
@@ -49,34 +15,31 @@ export const FILE_NAMES: Record<string, string> = {
 
   MUSIC_MAIN_MENU: "mainMenu.mp3",
 
-  TOWER_AMETHYST_1: `${TOWERS.AMETHYST}${TYPES.TOWER}${URL_NUMBER.ONE}.png`,
+  TOWER_AMETHYST_1: `amethystTower (1).png`,
   
   PROJECTILE_SAPPHIRE_1: "Iceball_84x9.png",
 };
 
 // prettier-ignore
 export const ASSET_LIST: string[][] = [
-  //LEVELS
+  //BUI
   [`${FILE_NAMES.DSLOGO}`, `${UI_URL}${FILE_NAMES.DSLOGO}`],
   
-  //GUI
+  //LEVELS
   [`${FILE_NAMES.LEVEL_LAVONEY}`, `${LEVELS_URL}${FILE_NAMES.LEVEL_LAVONEY}`],
-  // ['hellwortica',  './images/levels/hellwortica/hellwortica.png'],
-  // ['towerSpot',   './images/levels/towerSpot.png'],
-  // ['bossWave',    './audio/bossWave.ogg'],
-  // ['menuMusic',   './audio/menuMusic.mp3'],
-  // ['blood',       `${EFFECTS_URL}blood_110x110.png`],
-  // ['blueExplosion',   `${EFFECTS_URL}blueExplosion_256x256.png`],
-  // ['sapphireProjectile',    `${PROJECTILES_URL}sapphireProjectile_50x25.png`],
 
   //MUSIC 
   [`${FILE_NAMES.MUSIC_MAIN_MENU}`, `${MUSIC_URL}${FILE_NAMES.MUSIC_MAIN_MENU}`],
+
+  //SOUNDFX
+
   //ENEMIES
-  // [`${TYPES.ENEMY}${URL_NUMBER.ONE}`, `${ENEMIES_URL}${TYPES.ENEMY}${URL_NUMBER.ONE}.png`],
-  // [`${TYPES.ENEMY}${URL_NUMBER.TWO}`, `${ENEMIES_URL}${TYPES.ENEMY}${URL_NUMBER.TWO}.png`],
+
   //TOWERS
   [`${FILE_NAMES.TOWER_AMETHYST_1}`, `${TOWERS_URL}${FILE_NAMES.TOWER_AMETHYST_1}`],
 
   //PROJECTILES
   [`${FILE_NAMES.PROJECTILE_SAPPHIRE_1}`, `${PROJECTILES_URL}${FILE_NAMES.PROJECTILE_SAPPHIRE_1}`],
+
+  //EFFECTS
 ];
