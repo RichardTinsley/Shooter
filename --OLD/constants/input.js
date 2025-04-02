b56ce7b4eafed4618ff359469690e5bb8dd56370
RichardTinsley <49978658+RichardTinsley@users.noreply.github.com.>
2025-04-03 07:47:14 +1000

    TYPESCRIPT DAY 16 BEFORE NEW GIT REPO CLEAN UP

--- --OLD/constants/input.js
+++
@@ -1,8 +0,0 @@
-export const SIZE = 3;
-
-export const KEYBOARD = {
-    PAUSE: 'p',
-    DEBUG: 'o',
-    MUSIC: 'm',
-    RESTART: 'r'
-}
--- --OLD/utilities/array.js
+++
@@ -1,6 +0,0 @@
-export function create2DArray(tileMap, size){
-    const TileMapArray = [];
-    for (let i = 0; i < tileMap.length; i+= size)
-        TileMapArray.push(tileMap.slice(i, i + size));
-    return TileMapArray;
-}
