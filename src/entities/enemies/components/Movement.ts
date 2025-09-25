22935e791a52a830bb8af06f41e3092cbdca7b1e
RichardTinsley <49978658+RichardTinsley@users.noreply.github.com.>
2025-09-26 09:33:11 +1000

    Enemy Movement clean up

--- src/entities/enemies/components/Movement.ts
+++ src/entities/enemies/components/Movement.ts
@@ -1,6 +1,8 @@
 import { Position } from "../../../constants/types.js";
+import { HUD } from "../../../GUI/HUD/HUD.js";
 import { Level } from "../../../handlers/Level.js";
-import { enemyMovement } from "../../../utilities/entityMovement.js";
+import { checkCircleCollision } from "../../../utilities/collisionDetection.js";
+import { getDirection, setAngle, updatePosition } from "../../../utilities/entityMovement.js";
 import { randomFloat } from "../../../utilities/math.js";
 
 export class Movement {
@@ -13,7 +15,7 @@ export class Movement {
   private angle!: number;
 
   update(enemy: any) {
-    enemyMovement(enemy);
+    this.enemyMovement(enemy);
   }
 
   getWaypoints(): Position {
@@ -35,5 +37,30 @@ export class Movement {
     const dx = enemy.destination.y - enemy.position.y;
     const dy = enemy.destination.x - enemy.position.x;
     this.priorityDistance = Math.round(Math.abs(dx) + Math.abs(dy));
+  }
+
+  enemyMovement(enemy: any) {
+    if (checkCircleCollision(enemy.position, enemy.destination, 5, 10)) {
+      enemy.movement.waypointIndex++;
+
+      if (enemy.movement.waypointIndex === enemy.movement.waypoints.length) {
+        HUD.hudLives.setLives();
+        enemy.movement.waypointIndex = 0;
+        enemy.setPosition(enemy.movement.waypoints[enemy.movement.waypointIndex]);
+
+        enemy.sprite.setPosition(enemy.position);
+        enemy.hitDetection.setPosition(enemy.position);
+        enemy.healthBar.setPosition(enemy.position);
+      }
+
+      enemy.setDestination(
+        enemy.movement.waypoints[enemy.movement.waypointIndex]
+      );
+      enemy.movement.angle = setAngle(enemy.position, enemy.destination);
+      enemy.sprite.setDirection(getDirection(enemy.movement.angle));
+    }
+
+    updatePosition(enemy.position, enemy.movement.angle, enemy.movement.speed);
+    enemy.movement.setPriorityDistance(enemy);
   }
 }
--- src/utilities/entityMovement.ts
+++ src/utilities/entityMovement.ts
@@ -1,20 +1,18 @@
 import { Position } from "../constants/types.js";
-import { HUD } from "../GUI/HUD/HUD.js";
 import { Time } from "../handlers/Time.js";
-import { checkCircleCollision } from "./collisionDetection.js";
 
 export enum DIRECTION {
   LEFT = -1,
   RIGHT = 1,
 }
 
-export function setAngle(position: Position, destination: Position) {
+export function setAngle(position: Position, destination: Position):number  {
   const dy = destination.y - position.y;
   const dx = destination.x - position.x;
   return Math.atan2(dy, dx);
 }
 
-export function getDirection(angle: number) {
+export function getDirection(angle: number):number {
   if (angle < 1.57 && angle > -1.57) return DIRECTION.RIGHT;
   else return DIRECTION.LEFT;
 }
@@ -23,32 +21,7 @@ export function updatePosition(
   position: Position,
   angle: number,
   speed: number
-) {
+): void {
   position.x += Math.cos(angle) * speed * Time.deltaTimeMultiplier;
   position.y += Math.sin(angle) * speed * Time.deltaTimeMultiplier;
-}
-
-export function enemyMovement(enemy: any) {
-  if (checkCircleCollision(enemy.position, enemy.destination, 5, 10)) {
-    enemy.movement.waypointIndex++;
-
-    if (enemy.movement.waypointIndex === enemy.movement.waypoints.length) {
-      HUD.hudLives.setLives();
-      enemy.movement.waypointIndex = 0;
-      enemy.setPosition(enemy.movement.waypoints[enemy.movement.waypointIndex]);
-
-      enemy.sprite.setPosition(enemy.position);
-      enemy.hitDetection.setPosition(enemy.position);
-      enemy.healthBar.setPosition(enemy.position);
-    }
-
-    enemy.setDestination(
-      enemy.movement.waypoints[enemy.movement.waypointIndex]
-    );
-    enemy.movement.angle = setAngle(enemy.position, enemy.destination);
-    enemy.sprite.setDirection(getDirection(enemy.movement.angle));
-  }
-
-  updatePosition(enemy.position, enemy.movement.angle, enemy.movement.speed);
-  enemy.movement.setPriorityDistance(enemy);
 }
